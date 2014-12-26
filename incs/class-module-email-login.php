9936da39eaea18a716ca58ce7da81f2ee3f321a2
kuck1u <kuck1u@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-26 05:20:08 +0000

    Update.
    git-svn-id: https://plugins.svn.wordpress.org/va-simple-enhanced-security/tags/0.3.1@1054329 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ incs/class-module-email-login.php
@@ -0,0 +1,183 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+/**
+ * VA Simple Enhanced Security module - allow email login.
+ *
+ * @package WordPress
+ * @subpackage VA Simple Enhanced Security
+ * @author KUCKLU <kuck1u@visualive.jp>
+ *
+ * This source file is subject to the GNU GENERAL PUBLIC LICENSE (GPLv2.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+class VASES_MODUL_EMAIL_LOGIN {
+
+    /**
+     * Holds the singleton instance of this class
+     */
+    static $instance = false;
+
+    public $setting  = false;
+
+    /**
+     * Singleton
+     * @static
+     */
+    public static function init() {
+        if ( ! self::$instance ) {
+            self::$instance = new VASES_MODUL_EMAIL_LOGIN;
+        }
+
+        return self::$instance;
+    }
+
+    function __construct() {
+        $settings         = VASES_MODUL_SETTINGS_API::init();
+        $flug_email_login = $settings->args['email_login'];
+
+        if ( $flug_email_login == true ) {
+            $this->setting = true;
+            add_action( 'login_init', array( &$this, 'login_init'), 0 );
+        }
+    }
+
+    function login_init() {
+        $this->setting = true;
+        add_action( 'login_enqueue_scripts', array( &$this, 'login_enqueue_scripts' ) );
+        remove_filter( 'authenticate',       'wp_authenticate_username_password', 20, 3 );
+        add_filter( 'authenticate',          array( &$this, 'allow_email_login' ), 20, 3 );
+        add_filter( 'gettext',               array( &$this, 'changed_loginform_text' ), 20, 3 );
+        add_filter( 'wp_login_errors',       array( &$this, 'override_incorrect_password' ) );
+    }
+
+    function login_enqueue_scripts() {
+        wp_enqueue_style( 'dashicons' );
+        wp_enqueue_style( 'vases-login', VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_URL . 'assets/css/vases-login.css', array( 'dashicons' ) );
+        wp_print_styles();
+    }
+
+    /**
+     * Allow email login
+     * @param  object  $user
+     * @param  string  $username
+     * @param  string  $password
+     * @return boolean
+     */
+    function allow_email_login( $user, $username, $password ) {
+        if ( ! empty( $username ) && ! empty( $password ) && is_email( $username ) ) {
+            $user = get_user_by( 'email', $username );
+
+            if ( isset( $user, $user->user_login ) ) {
+                $username = $user->user_login;
+                return wp_authenticate_username_password( null, $username, $password );
+            } else {
+                return false;
+            }
+        }
+
+        if ( ! empty( $username ) || ! empty( $password ) ) {
+            return false;
+        } else {
+            return wp_authenticate_username_password( null, '', '' );
+        }
+    }
+
+    /**
+     * Changed login form text
+     * @param  string $translated_text translated text
+     * @param  string $text            original text
+     * @param  string $domain          text domain
+     * @return string
+     */
+    function changed_loginform_text( $translated_text, $text, $domain ) {
+        global $pagenow;
+
+        if ( is_user_logged_in() || 'wp-login.php' !== $pagenow ) {
+            return $translated_text;
+        }
+
+        if ( 'Username' === $translated_text ) {
+            $translated_text = 'Email address';
+        }
+        if ( 'ユーザー名' === $translated_text ) {
+            $translated_text = 'メールアドレス';
+        }
+
+        return $translated_text;
+    }
+
+    /**
+     * Override incorrect password message
+     * @param  object $errors
+     * @return object $errors
+     */
+    function override_incorrect_password( $errors ) {
+        if( isset( $errors->errors['incorrect_password'] ) ) {
+            $errors->errors = array( 'incorrect_password' => array( __( '<strong>ERROR</strong>: The password you entered for the username is incorrect.', VA_SIMPLE_ENHANCED_SECURITY_TEXTDOMAIN ) ) );
+        }
+
+        return $errors;
+    }
+}
+
+
+if ( VASES_MODUL_EMAIL_LOGIN::init()->setting == true && !function_exists( 'wp_authenticate' ) ) :
+/**
+ * Checks a user's login information and logs them in if it checks out.
+ * wp_authenticate of wp-includes/pluggable.php.
+ *
+ * @since 2.5.0
+ *
+ * @param string $username User's username
+ * @param string $password User's password
+ * @return WP_User|WP_Error WP_User object if login successful, otherwise WP_Error object.
+ */
+function wp_authenticate( $username, $password ) {
+    $username = sanitize_email( $username );
+    $password = trim( $password );
+
+    if ( ! empty( $username ) && ! empty( $password ) && is_email( $username ) ) {
+        $user = get_user_by( 'email', $username );
+        if ( isset( $user, $user->user_login ) ) {
+            $username = $user->user_login;
+        }
+    }
+
+    /**
+     * Filter the user to authenticate.
+     *
+     * If a non-null value is passed, the filter will effectively short-circuit
+     * authentication, returning an error instead.
+     *
+     * @since 2.8.0
+     *
+     * @param null|WP_User $user     User to authenticate.
+     * @param string       $username User login.
+     * @param string       $password User password
+     */
+    $user = apply_filters( 'authenticate', null, $username, $password );
+
+    if ( $user == null ) {
+        // TODO what should the error message be? (Or would these even happen?)
+        // Only needed if all authentication handlers fail to return anything.
+        $user = new WP_Error( 'authentication_failed', __( '<strong>ERROR</strong>: Invalid username or incorrect password.' ) );
+    }
+
+    $ignore_codes = array( 'empty_username', 'empty_password' );
+
+    if ( is_wp_error( $user ) && !in_array( $user->get_error_code(), $ignore_codes ) ) {
+        /**
+         * Fires after a user login has failed.
+         *
+         * @since 2.5.0
+         *
+         * @param string $username User login.
+         */
+        do_action( 'wp_login_failed', $username );
+    }
+
+    return $user;
+}
+endif;
---
+++ va-simple-enhanced-security.php
@@ -0,0 +1,124 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+/*
+Plugin Name: VA Simple Enhanced Security
+Plugin URI: https://github.com/VisuAlive/va-simple-enhanced-security
+Description: This plugin will enhance the security of your WordPress.
+Author: KUCKLU
+Version: 0.3.1
+Author URI: http://visualive.jp/
+Text Domain: va-simple-enhanced-security
+Domain Path: /langs
+GitHub Plugin URI: https://github.com/VisuAlive/va-simple-enhanced-security
+GitHub Branch: master
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+VisuAlive WordPress Plugin, Copyright (C) 2014 VisuAlive and KUCKLU.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * VA SIMPLE ENHANCED SECURITY
+ *
+ * @package WordPress
+ * @subpackage VA Simple Enhanced Security
+ * @since VA Simple Enhanced Security 0.0.1
+ * @author KUCKLU <kuck1u@visualive.jp>
+ * @copyright Copyright (c) 2014 KUCKLU, VisuAlive.
+ * @license http://opensource.org/licenses/gpl-2.0.php GPLv2
+ * @link http://visualive.jp/
+ */
+$vases_plugin_data = get_file_data( __FILE__, array('ver' => 'Version', 'langs' => 'Domain Path', 'mo' => 'Text Domain' ) );
+define( 'VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_URL',  plugin_dir_url(__FILE__) );
+define( 'VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_PATH', plugin_dir_path(__FILE__) );
+define( 'VA_SIMPLE_ENHANCED_SECURITY_DOMAIN',      dirname( plugin_basename(__FILE__) ) );
+define( 'VA_SIMPLE_ENHANCED_SECURITY_VERSION',     $vases_plugin_data['ver'] );
+define( 'VA_SIMPLE_ENHANCED_SECURITY_TEXTDOMAIN',  $vases_plugin_data['mo'] );
+
+/**
+ * Include files
+ */
+require_once( VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_PATH . 'incs/class-module-settings-api.php' );
+require_once( VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_PATH . 'incs/class-module-send-mail.php' );
+require_once( VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_PATH . 'incs/class-module-apis.php' );
+require_once( VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_PATH . 'incs/class-module-basic-auth.php' );
+require_once( VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_PATH . 'incs/class-module-digest-auth.php' );
+require_once( VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_PATH . 'incs/class-module-email-login.php' );
+require_once( VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_PATH . 'incs/class-module-author-base.php' );
+require_once( VA_SIMPLE_ENHANCED_SECURITY_PLUGIN_PATH . 'incs/class-module-etc.php' );
+
+class VA_SIMPLE_ENHANCED_SECURITY {
+
+    public $settings = array();
+
+    /**
+     * Holds the singleton instance of this class
+     */
+    static $instance = false;
+
+    /**
+     * Singleton
+     * @static
+     */
+    public static function init() {
+        if ( ! self::$instance ) {
+            self::$instance = new VA_SIMPLE_ENHANCED_SECURITY;
+        }
+
+        return self::$instance;
+    }
+
+    function __construct() {
+        $settings_api   = VASES_MODUL_SETTINGS_API::init();
+        $send_mail      = VASES_MODUL_SEND_MAIL::init();
+        $basic_auth     = VASES_MODUL_BASIC_AUTH::init();
+        $digest_auth    = VASES_MODUL_DIGEST_AUTH::init();
+        $email_login    = VASES_MODUL_EMAIL_LOGIN::init();
+        $author_base    = VASES_MODUL_AUTHOR_BASE::init();
+        $etc            = VASES_MODUL_ETC::init();
+
+        add_action( 'plugins_loaded', array( &$this, 'plugins_loaded' ) );
+    }
+
+    public function plugins_loaded() {
+        load_plugin_textdomain( VA_SIMPLE_ENHANCED_SECURITY_TEXTDOMAIN, false, VA_SIMPLE_ENHANCED_SECURITY_DOMAIN . '/langs' );
+    }
+
+    /**
+     * Activation
+     * @return void
+     */
+    public static function activation() {
+        VASES_MODUL_APIS::edit_htaccess( 'activation' );
+        flush_rewrite_rules();
+    }
+
+    /**
+     * Uninstall
+     * @return void
+     */
+    public static function uninstall() {
+        VASES_MODUL_APIS::edit_htaccess( 'uninstall' );
+        delete_option( 'vases_settings' );
+        delete_option( 'vases_http_auth_send_mail' );
+        wp_clear_scheduled_hook( 'vases_http_auth_send_mail' );
+        flush_rewrite_rules();
+    }
+}
+VA_SIMPLE_ENHANCED_SECURITY::init();
+
+register_activation_hook( __FILE__,   array( 'VA_SIMPLE_ENHANCED_SECURITY', 'activation' ) );
+register_deactivation_hook( __FILE__, array( 'VA_SIMPLE_ENHANCED_SECURITY', 'uninstall' ) );
+register_uninstall_hook( __FILE__,    array( 'VA_SIMPLE_ENHANCED_SECURITY', 'uninstall' ) );
